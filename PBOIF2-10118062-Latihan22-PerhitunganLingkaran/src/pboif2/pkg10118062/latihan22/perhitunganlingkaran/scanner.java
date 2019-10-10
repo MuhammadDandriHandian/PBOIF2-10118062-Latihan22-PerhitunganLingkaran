8cebd13c6648f19e36a1b88c93b6fcf94ae81861
MuhammadDandriHandian <55798941+MuhammadDandriHandian@users.noreply.github.com>
2019-10-10 15:04:13 +0700

    Add files via upload

---
+++ PBOIF2-10118062-Latihan22-PerhitunganLingkaran/src/pboif2/pkg10118062/latihan22/perhitunganlingkaran/scanner.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10118062.latihan22.perhitunganlingkaran;
+
+/**
+ *
+ * @author ASUS
+ */
+class scanner {
+
+    static void next() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
